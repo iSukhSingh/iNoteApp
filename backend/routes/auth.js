9e67e6f01176cb03a9b698937ceb7e481841449e
cali1204 <qwertyuiop.s1204@gmail.com>
2022-10-21 23:34:40 -0400

    Hashing Passwords using bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router(); 
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 
 // Create a User using: POST "/api/auth/". Doesn't require Auth
@@ -9,19 +10,33 @@ router.post('/',[
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
-] , (req, res)=>{ 
+] , async (req, res)=>{ 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-      }).then(user => res.json(user))
-      .catch(err=> {console.log(err)
-    res.json({error: 'Please enter a unique value for email', message: err.message})})
- 
-} )
+  // Check whether the user with this email exists already
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: "Sorry a user with this email already exists" })
+    }
+    // storep passwords in hash next two 
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+    // Create a new user
+    user = await User.create({
+      name: req.body.name,
+      // password: req.body.password,
+      password: secPass,
+      email: req.body.email,
+    })
+    res.json(user)
+    
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some Error occured");
+  }
+})
 
 module.exports = router
